b77e9777578d581883a4e903e1f5054542fe13c7
igmtdev <igmtdevofficial@gmail.com>
2024-09-11 23:40:09 +0800

    add nvim ts autotag plugin

---
+++ .config/nvim/lua/igmtdev/plugins/ts-autotag.lua
@@ -0,0 +1,9 @@
+return {
+  "windwp/nvim-ts-autotag",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    local autotag = require("nvim-ts-autotag")
+
+    autotag.setup()
+  end,
+}
